c1905fb045113770d27ef9af96b5dbf629028db7
ipeacocks <ipeacocks@gmail.com>
2017-05-20 00:57:57 +0300

    changed templates and added new css file

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ app.secret_key = 's3cr3t'
 
 @app.route("/", methods=['GET','POST'])
 def main():
+    error = None
     form = AnsibleForm(request.form)
     if request.method == 'POST':
-       print request.form['hostname']
-    return render_template('index.html', form=form)
+        if form.validate_on_submit():
+            print(request.form['hostname'])
+
+    return render_template('index.html', form=form, error=error)
 
 if __name__ == "__main__":
     app.run()
