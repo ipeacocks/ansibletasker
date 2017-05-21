074b6f7fe35c9c3c36e7e483a79cff0ba5063d31
ipeacocks <ipeacocks@gmail.com>
2017-05-21 21:49:54 +0300

    Simple flask output

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, Response
+import time
 from forms import AnsibleForm
 
 
@@ -13,9 +14,19 @@ def main():
     form = AnsibleForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
-            print(request.form['hostname'])
-
+            # print(request.form['hostname'])
+            return redirect(url_for('output'))
     return render_template('index.html', form=form, error=error)
+
+
+@app.route("/output")
+def output():
+    def inner():
+        for x in range(100):
+            time.sleep(1)
+            yield '%s<br/>\n' % x
+    return Response(inner(), mimetype='text/html')  # text/html is required for most browsers to show the partial page immediately
+
 
 if __name__ == "__main__":
     app.run()
