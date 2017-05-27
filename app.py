42a9591d22cb9b86280ab382fcf25fc9eef20c44
ipeacocks <ipeacocks@gmail.com>
2017-05-27 23:33:35 +0300

    Made ansible task output

--- app.py
+++ app.py
@@ -19,29 +19,9 @@ def main():
     form = AnsibleForm(request.form)
     if request.method == 'POST':
         if form.validate_on_submit():
-            my_host = request.form['hostname']
-            # return redirect(url_for('output', host=my_host))
-            return redirect(url_for('index'))
+            host = request.form['hostname']
+            return redirect(url_for('index', host=host))
     return render_template('index.html', form=form, error=error)
-
-
-# @app.route('/output')
-# def output():
-#     host = request.args['host']
-#     def inner():
-#         ansible_command="ansible-playbook -i ansible/hosts ansible/user.yml --limit {}".format(host)
-#         proc = subprocess.Popen(
-#             [ansible_command],
-#             shell=True,
-#             stdout=subprocess.PIPE,
-#             universal_newlines=True
-#         )
-
-#         for line in iter(proc.stdout.readline, ''):
-#             time.sleep(1)
-#             yield line.rstrip()+'<br/>\n'
-
-#     return Response(inner(), mimetype='text/html')
 
 
 @app.route('/output')
@@ -52,11 +32,19 @@ def index():
 
 @app.route('/stream_sqrt')
 def stream():
+    host = request.args['host']
     def generate():
-        for i in range(500):
-            yield '{}\n'.format(sqrt(i))
+        ansible_command="ansible-playbook -i ansible/hosts ansible/user.yml --limit {}".format(host)
+        proc = subprocess.Popen(
+            [ansible_command],
+            shell=True,
+            stdout=subprocess.PIPE,
+            universal_newlines=True
+        )
+        for line in iter(proc.stdout.readline, ''):
             sleep(1)
-
+            yield '{}\n'.format(line.rstrip())
+            
     return app.response_class(generate(), mimetype='text/plain')
 
 
