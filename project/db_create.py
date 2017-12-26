09ec4379361eb705fed06c979ee08a17ea73d6cc
Yurii Bychenok <ipeacocks@gmail.com>
2017-12-26 14:25:09 +0200

    Converted tabs to spaces

--- project/db_create.py
+++ project/db_create.py
@@ -8,12 +8,12 @@ from datetime import date
 db.create_all()
 # login/pass - admin/admin
 db.session.add(
-	User(
-		date(2016, 2, 23),
-		'admin',
-		'admin@example.com',
-		'$2b$12$/6k5KPJ.BjICuNh8AyrQZeb9JlsgBt3p/LrWEha6pkvnYs9SBc9u.',
-		'admin'
-	)
+    User(
+        date(2016, 2, 23),
+        'admin',
+        'admin@example.com',
+        '$2b$12$/6k5KPJ.BjICuNh8AyrQZeb9JlsgBt3p/LrWEha6pkvnYs9SBc9u.',
+        'admin'
+    )
 )
 db.session.commit()
